3d7c2052a028d1e214d12515b90df3fc03c595a0
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-06-14 20:52:32 +0200

    test: fix deno tests when cwd is not the project root

--- tests/deno.ts
+++ tests/deno.ts
@@ -1,28 +1,26 @@
 import * as TOML from "../web/toml2js.js";
 
-const PROJECT_ROOT = ".";
+const PROJECT_ROOT = new URL("..", import.meta.url);
 
-await TOML.default(await Deno.readFile(PROJECT_ROOT + "/web/toml2js_bg.wasm"));
+await TOML.default(
+  await Deno.readFile(new URL("web/toml2js_bg.wasm", PROJECT_ROOT))
+);
 
 Deno.test({
   name: "simpleTranscription",
   async fn(): Promise<void> {
     const [actual, expected] = await Promise.all([
-      readFile(PROJECT_ROOT + "/tests/input.toml").then(TOML.parse),
-      readFile(PROJECT_ROOT + "/tests/output.json").then(JSON.parse),
+      Deno.readTextFile(new URL("tests/input.toml", PROJECT_ROOT)).then(
+        TOML.parse
+      ),
+      Deno.readTextFile(new URL("tests/output.json", PROJECT_ROOT)).then(
+        JSON.parse
+      ),
     ]);
     if (!isDeepEqual(actual, expected))
       return Promise.reject(new Error("ASSERTION ERROR: test data mismatch."));
   },
 });
-
-const utf8Decoder = new TextDecoder("utf-8");
-async function readFile(path: string) {
-  const input = await Deno.open(path);
-  const result = await Deno.readAll(input);
-  input.close();
-  return utf8Decoder.decode(result);
-}
 
 function isDeepEqual(actual: any, expected: any): boolean {
   if (actual === expected) return true;
